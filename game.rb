e020a497d62c300962575b462dd0c9f7859f7567
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-11-30 10:21:29 -0700

    Window start, update, draw methods add

---
+++ game.rb
@@ -0,0 +1,23 @@
+require "gosu"
+
+class Game < Gosu::Window
+
+	WIDTH = 640
+	HEIGHT = 480
+
+	def initialize 
+		super(WIDTH, HEIGHT)
+	end
+
+	def update
+
+	end
+
+	def draw
+
+	end
+
+end
+
+game = Game.new
+game.show

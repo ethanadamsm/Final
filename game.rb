039d2db9d2bff97f20d7604f6a7010bc3a5df632
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-12-01 10:20:32 -0700

    Add menu background and play button

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require "gosu"
 require_relative "grid"
+require_relative "button"
 
 class Game < Gosu::Window
 
@@ -9,6 +10,10 @@ class Game < Gosu::Window
 	def initialize 
 		super(WIDTH, HEIGHT)
 		@grid = Grid.new(0)
+		@menu = true
+		@level = false
+		@bg = Gosu::Image.new("background.png")
+		@button_play = Button.new(168, 200, 400, 100, "button.png", "Play", 30, false)
 	end
 
 	def update
@@ -16,7 +21,14 @@ class Game < Gosu::Window
 	end
 
 	def draw
-		@grid.draw
+		if @level
+			@grid.draw
+		end
+
+		if @menu
+			@bg.draw(0, 0, 0)
+			@button_play.draw
+		end
 	end
 
 end

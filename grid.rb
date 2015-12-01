684f8cc7fb7a229a2c55022bcea23ab7c6c000a2
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-12-01 10:09:35 -0700

    Create tile objects

--- grid.rb
+++ grid.rb
@@ -1,14 +1,28 @@
+require_relative "tile"
+
 class Grid
 
-	attr_reader :width, :height
-
-	def initialize(width, height)
-		@width = width
-		@height = height
+	def initialize(level)
+		file = File.open("#{level}.txt")
+		file = file.read
+		tiles = file.split(" ")
+		@tile_objects = []
+		x = 0;
+		y = 0;
+		(0...tiles.length).each do |n|
+			@tile_objects.push(Tile.new(x, y, tiles[n]))
+			x += 32
+			if x >= 736
+				x = 0
+				y += 32
+			end
+		end
 	end
 
 	def draw
-		
+		@tile_objects.each do |tile|
+			tile.draw
+		end
 	end
 
 end

b1598e7e1decc271fd3f4ac0abde76beebb06edc
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-12-02 11:04:44 -0700

    Add player

--- grid.rb
+++ grid.rb
@@ -7,9 +7,14 @@ class Grid
 		file = file.read
 		tiles = file.split(" ")
 		@tile_objects = []
+		@player_spot = nil
+		#@player_image = Gosu::Image("player.png")
 		x = 0;
 		y = 0;
 		(0...tiles.length).each do |n|
+			if tiles[n] == "1"
+				@player_spot = n
+			end
 			@tile_objects.push(Tile.new(x, y, tiles[n]))
 			x += 32
 			if x >= 736
